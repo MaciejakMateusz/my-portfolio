a819765ee49c48eb393bd94d90316109e9322b68
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-02-13 20:37:02 +0100

    Updated Introduction component structure

--- src/main/landing/Introduction.tsx
+++ src/main/landing/Introduction.tsx
@@ -1,7 +1,18 @@
+import {useTranslation} from "react-i18next";
+
 export const Introduction = () => {
+    const {t} = useTranslation();
     return (
         <div className={'introduction-container'}>
-            <h1>Elo</h1>
+            <div>
+                <span className={'introduction-welcome'}>{t('hiIm')} <span className={'my-name'}>{t('myName')}</span></span>
+                <span className={'introduction-full-stack'}>Full Stack</span>
+                <span className={'introduction-developer'}>Developer</span>
+                <span className={'introduction-description'}>
+                Jestem młodszym full-stack developerem, który potrafi tworzyć wysokiej jakości aplikacje webowe. Moje umiejętności obejmują Spring, React, SQL oraz wiele innych technologii. Moje zaangażowanie w stosowanie najlepszych praktyk programistycznych.
+                </span>
+                <div className={'contact-button'}>Skontaktuj się</div>
+            </div>
         </div>
     );
 }
