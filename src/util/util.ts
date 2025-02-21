841b101de4c5cee84bb9264d223ca5cc9155ff98
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-02-21 19:19:06 +0100

    Added translations for months and created getMonth() arrow function in util.ts

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,4 +1,4 @@
-import i18n from "i18next";
+import i18n, {TFunction} from "i18next";
 
 export const getCookie = (cookieName: string) => {
     let cookie: any = {};
@@ -12,4 +12,35 @@ export const getCookie = (cookieName: string) => {
 export const getLanguage = () => {
     const lngCookie = getCookie('lng');
     return lngCookie ? lngCookie : i18n.language;
+}
+
+export const getMonth = (month: number, t: TFunction) => {
+    switch (month) {
+        case 1:
+            return t('january')
+        case 2:
+            return t('february')
+        case 3:
+            return t('march')
+        case 4:
+            return t('april')
+        case 5:
+            return t('may')
+        case 6:
+            return t('june')
+        case 7:
+            return t('july')
+        case 8:
+            return t('august')
+        case 9:
+            return t('september')
+        case 10:
+            return t('october')
+        case 11:
+            return t('november')
+        case 12:
+            return t('december')
+        default:
+            return "Month"
+    }
 }
