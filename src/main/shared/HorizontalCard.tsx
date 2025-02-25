78a3d83b3bc30052dd6934f26444ad5a6de3eac7
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-02-25 18:57:05 +0100

    Updated HorizontalCard to handle clickable descriptions

--- src/main/shared/HorizontalCard.tsx
+++ src/main/shared/HorizontalCard.tsx
@@ -4,16 +4,30 @@ type HobbyCardProps = {
     header: string;
     description: string;
     icon: string;
+    type?: string;
+    href?: string;
 }
 
 export const HorizontalCard = (props: HobbyCardProps) => {
+
+    const handleClick = () => {
+        if (!props.type || !props.href) {
+            return;
+        } else if (props.type === 'map') {
+            window.open(props.href, '_blank')
+            return;
+        }
+        window.location.href = props.href;
+    }
+
     return (
         <div className={'hobby-card-wrapper'}>
             <div className={'hobby-card-icon-container'}>
                 <ReactSVG src={props.icon}/>
             </div>
             <div className={'hobby-card-header'}>{props.header}</div>
-            <div className={'hobby-card-description'}>{props.description}</div>
+            <div className={`hobby-card-description ${props.href ? 'contact-type' : ''}`}
+                 onClick={() => handleClick()}>{props.description}</div>
         </div>
     );
 }
