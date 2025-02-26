ee696fe7b59a18045cdc6414a8ccd44e161f57dd
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-02-26 20:42:27 +0100

    Updated ProjectCard component

--- src/main/projects/ProjectCard.tsx
+++ src/main/projects/ProjectCard.tsx
@@ -1,12 +1,16 @@
+import {useTranslation} from "react-i18next";
 
 type ProjectCardProps = {
     name: string;
     description: string;
     technologies: string;
     img: string;
+    projectUrl?: string | undefined;
+    githubUrl?: string | undefined;
 };
 
 export const ProjectCard = (props: ProjectCardProps) => {
+    const {t} = useTranslation();
     return (
         <div className="carousel-card">
             <div className="carousel-card-inner-wrapper">
@@ -21,11 +25,14 @@ export const ProjectCard = (props: ProjectCardProps) => {
             <div className="carousel-card-turned">
                 <p className={'project-card-description'}>{props.description}</p>
                 <div className={'project-card-technologies-container'}>
-                    <p className={'project-card-technologies'}>Technologie:</p>
+                    <p className={'project-card-technologies'}>{t('technologies')}:</p>
                     <span>{props.technologies}</span>
                 </div>
                 <div className={'carousel-card-button-container'}>
-                    <button className={'primary-button'}>Zobacz projekt</button>
+                    <button className={'primary-button'}
+                            onClick={() => window.open(props.projectUrl, '_blank')}>
+                        {t('seeProject')}
+                    </button>
                 </div>
             </div>
         </div>
