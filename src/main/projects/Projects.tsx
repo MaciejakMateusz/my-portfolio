a6f6c915ed41a61bb5047bcb68c4a020d7d91156
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-02-26 18:47:18 +0100

    Created useProjectCards hook and used InfiniteXScroll in Projects component

--- src/main/projects/Projects.tsx
+++ src/main/projects/Projects.tsx
@@ -2,13 +2,15 @@ import {SectionHeader} from "../shared/SectionHeader.tsx";
 import {useTranslation} from "react-i18next";
 import {useInView} from "react-intersection-observer";
 import { motion } from "framer-motion";
-import {ProjectsCarousel} from "./ProjectsCarousel.tsx";
 import {forwardRef} from "react";
+import {InfiniteXScroll} from "./InfiniteXScroll.tsx";
+import {useProjectCards} from "../../hooks/useProjectCards.tsx";
 
 
 export const Projects =  forwardRef((_, ref: any) => {
     const {t} = useTranslation();
     const { ref: motionRef, inView } = useInView({ triggerOnce: true, threshold: 0.2 });
+    const projectCards = useProjectCards();
     return (
         <div ref={ref}>
             <motion.div
@@ -18,7 +20,7 @@ export const Projects =  forwardRef((_, ref: any) => {
                 transition={{duration: 0.8, ease: "easeOut"}}>
                 <section className={'projects'}>
                     <SectionHeader title={t('projects')} description={t('projectsDescription')}/>
-                    <ProjectsCarousel/>
+                    <InfiniteXScroll children={projectCards}/>
                 </section>
             </motion.div>
         </div>
