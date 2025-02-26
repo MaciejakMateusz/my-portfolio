271073f38881282ffdf2e14f22ef73c47c918401
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-02-26 20:42:40 +0100

    Updated Router with projects paths

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -9,6 +9,9 @@ export const Router = () => {
             <Routes>
                 <Route path='/' element={<Main />}/>
                 <Route path='/air-quality' element={<AirQuality />}/>
+                <Route path='/ai-translator' element={<h1>AI TRANSLATOR</h1>}/>
+                <Route path='/tolerance-measure' element={<h1>TOLERANCE MEASURE</h1>}/>
+                <Route path='/portfolio-api' element={<h1>PORTFOLIO REST API</h1>}/>
             </Routes>
         </BrowserRouter>
     )
