26d4b9e9aa6633ff21d1d95a2cd350be3dbdd7b9
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-01 19:14:50 +0100

    Created NumberField

---
+++ src/main/shared/form/NumberField.tsx
@@ -0,0 +1,19 @@
+import {useAppDispatch} from "../../../hooks/hooks.ts";
+import {NumberFieldProps} from "../../../types/NumberFieldProps.ts";
+
+export const NumberField = (props: NumberFieldProps) => {
+    const dispatch = useAppDispatch();
+    return (
+        <label className={'form-field-label'}>
+            {props.label}
+            <input type={'number'}
+                   {...props.register(props.name)}
+                   name={props.name}
+                   step={0.01}
+                   className={`form-text-field ${props.error ? 'invalid' : ''}`}
+                   value={props.value}
+                   onChange={(e) => dispatch(props.action(e.target.value))}/>
+            {props.error && <span className="validation-msg">{props.error.message}</span>}
+        </label>
+    );
+}
