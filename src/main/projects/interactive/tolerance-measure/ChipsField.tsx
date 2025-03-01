ca871902738fc95a4bcf22d95fbf3460c5dbba3a
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-01 19:15:19 +0100

    Created custom implementation of CreatableSelect - ChipsField

---
+++ src/main/projects/interactive/tolerance-measure/ChipsField.tsx
@@ -0,0 +1,56 @@
+import {forwardRef, KeyboardEvent, useState} from 'react';
+import CreatableSelect from 'react-select/creatable';
+import {useAppDispatch} from "../../../../hooks/hooks.ts";
+import {Chip} from "../../../../interfaces/Chip.ts";
+import {useTranslation} from "react-i18next";
+import {ChipsFieldProps} from "../../../../types/ChipsFieldProps.ts";
+
+export const ChipsField = forwardRef((props: ChipsFieldProps, ref: any) => {
+    const {t} = useTranslation();
+    const dispatch = useAppDispatch();
+    const [inputValue, setInputValue] = useState('');
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+        if (!inputValue) return;
+
+        if (['Enter', 'Tab', ','].includes(event.key)) {
+            event.preventDefault();
+            const trimmedInput = inputValue.trim();
+            if (trimmedInput) {
+                const numValue = Number(trimmedInput);
+                if (!isNaN(numValue)) {
+                    const newChip: Chip = { id: Date.now(), value: numValue, label: trimmedInput };
+                    const updatedChips = [...props.chips, newChip];
+                    props.onChange(updatedChips);
+                    dispatch(props.setChips(updatedChips));
+                }
+            }
+            setInputValue('');
+        }
+    };
+
+    return (
+        <label className={'form-field-label'}>
+            {t('measurements')}
+            <CreatableSelect
+                className={'chips-multiselect'}
+                isMulti
+                inputValue={inputValue}
+                onInputChange={(value) => setInputValue(value)}
+                ref={ref}
+                name={props.name}
+                onKeyDown={handleKeyDown}
+                onChange={(selectedOptions) => {
+                    const updatedChips = selectedOptions || [];
+                    props.onChange(updatedChips);
+                    dispatch(props.setChips(updatedChips));
+                }}
+                value={props.chips}
+                menuIsOpen={false}
+                getOptionValue={(option) => option.id.toString()}
+            />
+            {props.error && <span className="validation-msg">{props.error.message}</span>}
+        </label>
+
+    );
+});
