a7ff8e8336f755026d19bac3f1d6aa36c4fbe96c
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-01 08:03:57 +0100

    Updated and attached LanguageSwitcher to Start component

--- src/locales/LanguageSwitcher.tsx
+++ src/locales/LanguageSwitcher.tsx
@@ -1,7 +1,9 @@
 import { useTranslation } from 'react-i18next';
+import {getLanguage} from "../util/util.ts";
 
 export const LanguageSwitcher = () => {
     const { i18n } = useTranslation();
+    const activeLng = getLanguage();
 
     const changeLanguage = (lng :string) => {
         i18n.changeLanguage(lng);
@@ -9,9 +11,18 @@ export const LanguageSwitcher = () => {
     };
 
     return (
-        <div>
-            <button onClick={() => changeLanguage('en')}>English</button>
-            <button onClick={() => changeLanguage('pl')}>Polski</button>
+        <div className={'lang-switcher'}>
+            <div className={`lang-switcher-btn ${activeLng === 'pl' ? 'active-lng' : ''}`}>
+                <span onClick={() => changeLanguage('pl')}>Polski</span>
+            </div>
+            &nbsp;|&nbsp;
+            <div className={`lang-switcher-btn ${activeLng === 'en' ? 'active-lng' : ''}`}>
+                <span onClick={() => changeLanguage('en')}>English</span>
+            </div>
+            &nbsp;|&nbsp;
+            <div className={`lang-switcher-btn ${activeLng === 'nl' ? 'active-lng' : ''}`}>
+                <span onClick={() => changeLanguage('nl')}>Nederlands</span>
+            </div>
         </div>
     );
 };
