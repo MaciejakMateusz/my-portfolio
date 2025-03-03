6fd5301b72e95b337c7a414d3ed11ed3ac3e2a7b
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-03 21:27:44 +0100

    Created functional TranslatorAi component (no UI)

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -4,6 +4,7 @@ import {Main} from "../main/Main.tsx";
 import {AirQuality} from "../main/projects/interactive/air-quality/AirQuality.tsx";
 import {PortfolioApi} from "../main/projects/non-interactive/portfolio-api/PortfolioApi.tsx";
 import {ToleranceMeasure} from "../main/projects/interactive/tolerance-measure/ToleranceMeasure.tsx";
+import {TranslatorAI} from "../main/projects/interactive/translator-ai/TranslatorAI.tsx";
 
 export const Router = () => {
     return (
@@ -11,7 +12,7 @@ export const Router = () => {
             <Routes>
                 <Route path='/' element={<Main />}/>
                 <Route path='/air-quality' element={<AirQuality />}/>
-                <Route path='/ai-translator' element={<h1>AI TRANSLATOR</h1>}/>
+                <Route path='/ai-translator' element={<TranslatorAI/>}/>
                 <Route path='/tolerance-measure' element={<ToleranceMeasure/>}/>
                 <Route path='/portfolio-api' element={<PortfolioApi/>}/>
             </Routes>
