0a628222bb83be0edcec058d9ed7fa3c10e900b2
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-06 19:15:20 +0100

    Added 'lightened' prop to SectionHeader

--- src/main/shared/SectionHeader.tsx
+++ src/main/shared/SectionHeader.tsx
@@ -1,13 +1,14 @@
 type SectionHeaderType = {
-    title: string,
-    description: string
+    title: string;
+    description: string;
+    lightened?: boolean;
 }
 
-export const SectionHeader = ({title, description}: SectionHeaderType) => {
+export const SectionHeader = ({title, description, lightened}: SectionHeaderType) => {
     return (
         <div className={'section-header'}>
             <div className={'centered-container'}>
-                <img src={'/blurred-light.png'} className={'blurred-light'} alt={'Blurred light'}/>
+                {lightened && <img src={'/blurred-light.png'} className={'blurred-light'} alt={'Blurred light'}/>}
                 <div className={'centered-container-wrapper'}>
                     <span className={'section-header-title'}>{title}</span>
                     <span className={'section-header-description'}>{description}</span>
