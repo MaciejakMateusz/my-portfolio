d6bf0fa8ed451bd34a989d8bcf626b555d56ab53
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-08 08:40:59 +0100

    Updated About section - tablet RWD

--- src/main/about/AboutHobbiesChunk.tsx
+++ src/main/about/AboutHobbiesChunk.tsx
@@ -4,14 +4,18 @@ export const AboutHobbiesChunk = () => {
     const {t} = useTranslation();
     return (
         <div className={'about-hobbies-container main-typography'}>
-            <p className={'about-description-header'}>{t('guitar')}</p>
-            <p className={'about-description'}>
-                {t('guitarDescription')}
-            </p>
-            <p className={'about-description-header'}>Gaming</p>
-            <p className={'about-description'}>
-                {t('gamingDescription')}
-            </p>
+            <div className={'hobby-container'}>
+                <p className={'about-description-header'}>{t('guitar')}</p>
+                <p className={'about-description'}>
+                    {t('guitarDescription')}
+                </p>
+            </div>
+            <div className={'hobby-container'}>
+                <p className={'about-description-header'}>Gaming</p>
+                <p className={'about-description'}>
+                    {t('gamingDescription')}
+                </p>
+            </div>
         </div>
     );
 }
