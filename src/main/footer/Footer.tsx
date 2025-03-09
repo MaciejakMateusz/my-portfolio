7c54ab5e063b03650c92e2bb57a6143d8a7f6e3c
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-09 08:59:11 +0100

    Updated Footer mobile RWD

--- src/main/footer/Footer.tsx
+++ src/main/footer/Footer.tsx
@@ -1,10 +1,21 @@
 import {useTranslation} from "react-i18next";
+import {useEffect, useState} from "react";
 
 export const Footer = () => {
     const {t} = useTranslation();
+    const [isMobile, setIsMobile] = useState(false);
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 450);
+        };
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <footer className={'footer'}>
-            <span>&copy; {new Date().getFullYear()} Mateusz Maciejak. {t('allRightsReserved')}</span>
+            <span>&copy; {new Date().getFullYear()} Mateusz Maciejak. {isMobile && <br/>} {t('allRightsReserved')}</span>
         </footer>
     );
 }
