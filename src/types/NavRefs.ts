370e8710c7b8eeaa30e4adafe6886ae40c05bc14
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-09 08:50:28 +0100

    Created NavRefs type - preparation for navigation refactoring

---
+++ src/types/NavRefs.ts
@@ -0,0 +1,11 @@
+import {MutableRefObject} from "react";
+
+export type NavRefs = {
+    start: MutableRefObject<any>;
+    techStack: MutableRefObject<any>;
+    projects: MutableRefObject<any>;
+    activity: MutableRefObject<any>;
+    career: MutableRefObject<any>;
+    about: MutableRefObject<any>;
+    contact: MutableRefObject<any>;
+}
