107dfcb8d93ea465b7aae6f8b9fd0fc21fe43953
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-13 17:13:11 +0100

    Updated hungry-scan-api.json structure, moved json file to a better named file structure

--- src/main/projects/non-interactive/hungry-scan/HungryScan.tsx
+++ src/main/projects/non-interactive/hungry-scan/HungryScan.tsx
@@ -3,7 +3,7 @@ import SwaggerUI from "swagger-ui-react";
 export const HungryScan = () => {
     return (
         <div>
-            <SwaggerUI url="/json/hungry-scan-open-api.json" />
+            <SwaggerUI url="/open-api/json/hungry-scan-api.json" />
         </div>
     );
 };
--- src/main/projects/non-interactive/portfolio-api/PortfolioApi.tsx
+++ src/main/projects/non-interactive/portfolio-api/PortfolioApi.tsx
@@ -4,7 +4,7 @@ import "swagger-ui-react/swagger-ui.css";
 export const PortfolioApi = () => {
     return (
         <div>
-            <SwaggerUI url="/json/portfolio-api.json" />
+            <SwaggerUI url="/open-api/json/portfolio-api.json" />
         </div>
     );
 };
