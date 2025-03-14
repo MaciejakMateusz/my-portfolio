bd47646c0d2452210ec75633ec854bf8491d8362
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-14 20:10:13 +0100

    Decorated MotionWrapper with onYZero callback so it passes information when y = 0

--- src/main/shared/MotionWrapper.tsx
+++ src/main/shared/MotionWrapper.tsx
@@ -1,22 +1,42 @@
-import {motion} from 'framer-motion';
-import {LegacyRef} from "react";
+import {motion, useAnimation} from 'framer-motion';
+import {LegacyRef, useEffect} from "react";
 
 type MotionWrapperProps = {
     children: JSX.Element;
     motionRef: LegacyRef<HTMLDivElement> | undefined;
     inView: boolean;
-    className?: string;
     initialY: number;
+    onYZero?: (isYZero: boolean) => void;
 }
 
-export const MotionWrapper = ({children, motionRef, inView, initialY, className, ...props}: MotionWrapperProps) => {
+export const MotionWrapper = ({
+                                  children,
+                                  motionRef,
+                                  inView,
+                                  initialY,
+                                  onYZero,
+                                  ...props
+                              }: MotionWrapperProps) => {
+    const controls = useAnimation();
+
+    useEffect(() => {
+        if (inView) {
+            controls.start({ opacity: 1, y: 0 }).then(() => {
+                if (onYZero) onYZero(true);
+            });
+        } else {
+            controls.start({ opacity: 0, y: initialY });
+            if (onYZero) onYZero(false);
+        }
+    }, [inView, controls, initialY, onYZero]);
+
     return (
         <motion.div
             ref={motionRef}
-            initial={{opacity: 0, y: initialY}}
-            animate={inView ? {opacity: 1, y: 0} : {}}
-            transition={{duration: 0.8, ease: "easeOut"}}
-            className={className}
+            initial={{ opacity: 0, y: initialY }}
+            animate={controls}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            className={''}
             {...props}>
             {children}
         </motion.div>
