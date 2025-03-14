a020336efbd2174463ff1bf65188f0e6e3fc43ad
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-14 18:44:15 +0100

    Created useInCustomView hook that implements repeatable config from useInView hook

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,4 +1,6 @@
 import { useDispatch } from 'react-redux';
 import type { AppDispatch } from '../store/store.ts';
+import {useInView} from "react-intersection-observer";
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useInCustomView = () => useInView({triggerOnce: true, threshold: 0.2});
