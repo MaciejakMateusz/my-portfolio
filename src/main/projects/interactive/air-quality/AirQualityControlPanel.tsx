917d8b46aac775cf38cdc33cf14136f7fe61689f
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-21 21:02:51 +0100

    Updated AirQualityControlPanel mobile RWD

--- src/main/projects/interactive/air-quality/AirQualityControlPanel.tsx
+++ src/main/projects/interactive/air-quality/AirQualityControlPanel.tsx
@@ -65,66 +65,64 @@ export const AirQualityControlPanel = () => {
             <header className={'aq-control-panel'}>
                 <div className={'aq-locality-controls'}>
                     <div className={'aq-select'}>
-                        <label className={'custom-select-label'}>
-                            {t('country')}:
-                            <Select id="aq-country"
-                                    name="aq-country"
-                                    value={chosenCountry}
-                                    options={countriesData?.results?.map((data: any) => ({
-                                        value: data.id, label: data.name
-                                    }))}
-                                    noOptionsMessage={() => t('noOptionsMsg')}
-                                    onChange={(selected) => dispatch(setChosenCountry(selected))}
-                                    styles={secondarySelect}
-                                    isSearchable={true}
-                            />
-                        </label>
-                    </div>
-                    <div className={'aq-select'}>
-                        <label className={'custom-select-label'}>
-                            {t('location')}:
-                            <Select
-                                id="aq-location"
-                                name="aq-location"
-                                value={chosenLocation}
-                                options={locationsData?.results?.map((data: any) => ({
-                                    value: data, label: data.name
+                        <div className={'aq-range-label'}>{t('country')}:</div>
+                        <Select id="aq-country"
+                                name="aq-country"
+                                value={chosenCountry}
+                                options={countriesData?.results?.map((data: any) => ({
+                                    value: data.id, label: data.name
                                 }))}
                                 noOptionsMessage={() => t('noOptionsMsg')}
-                                onChange={(selected) => dispatch(setChosenLocation(selected))}
+                                onChange={(selected) => dispatch(setChosenCountry(selected))}
                                 styles={secondarySelect}
                                 isSearchable={true}
-                            />
-                        </label>
+                        />
+                    </div>
+                    <div className={'aq-select'}>
+                        <div className={'aq-range-label'}>{t('location')}:</div>
+                        <Select
+                            id="aq-location"
+                            name="aq-location"
+                            value={chosenLocation}
+                            options={locationsData?.results?.map((data: any) => ({
+                                value: data, label: data.name
+                            }))}
+                            noOptionsMessage={() => t('noOptionsMsg')}
+                            onChange={(selected) => dispatch(setChosenLocation(selected))}
+                            styles={secondarySelect}
+                            isSearchable={true}
+                        />
                     </div>
                 </div>
                 <div className={'aq-range-control'}>
-                    <div className={'aq-select'}>
-                        <label className={'custom-select-label'}>
-                            {t('date')}:
-                            <Select
-                                id="aq-year"
-                                name="aq-year"
-                                value={chosenYear}
-                                options={getYears()}
-                                noOptionsMessage={() => t('noOptionsMsg')}
-                                onChange={(selected) => dispatch(setChosenYear(selected))}
-                                styles={secondarySelectShort}
-                                isSearchable={true}
-                            />
-                        </label>
-                    </div>
-                    <div className={'aq-select'}>
-                            <Select
-                                id="aq-month"
-                                name="aq-month"
-                                value={chosenMonth}
-                                options={getMonths()}
-                                noOptionsMessage={() => t('noOptionsMsg')}
-                                onChange={(selected) => dispatch(setChosenMonth(selected))}
-                                styles={secondarySelectShort}
-                                isSearchable={false}
-                            />
+                    <div className={'aq-range-control-wrapper'}>
+                        <div className={'aq-range-date-label'}>{t('date')}:</div>
+                        <div className={'aq-range-control-selects'}>
+                            <div className={'aq-select'}>
+                                <Select
+                                    id="aq-year"
+                                    name="aq-year"
+                                    value={chosenYear}
+                                    options={getYears()}
+                                    noOptionsMessage={() => t('noOptionsMsg')}
+                                    onChange={(selected) => dispatch(setChosenYear(selected))}
+                                    styles={secondarySelectShort}
+                                    isSearchable={true}
+                                />
+                            </div>
+                            <div className={'aq-select'}>
+                                <Select
+                                    id="aq-month"
+                                    name="aq-month"
+                                    value={chosenMonth}
+                                    options={getMonths()}
+                                    noOptionsMessage={() => t('noOptionsMsg')}
+                                    onChange={(selected) => dispatch(setChosenMonth(selected))}
+                                    styles={secondarySelectShort}
+                                    isSearchable={false}
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </header>
